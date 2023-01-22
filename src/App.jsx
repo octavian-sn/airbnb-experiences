85c8f1d9114016dbd5eb7bf874ce12f828947dda
Sulic Octavian <octavius1992@gmail.com>
2023-01-22 09:14:03 +0200

    Add nav component and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import Nav from './components/Nav';
 
 class App extends Component {
   render() {
-    return <div>Just a div</div>;
+    return (
+      <div>
+        <Nav />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import logo from '../assets/airbnb.png';
+
+export default function Nav() {
+  return (
+    <div className="nav">
+      <img src={logo} alt="airbnb logo" />
+    </div>
+  );
+}
